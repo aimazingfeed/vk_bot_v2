8ae61a2794b99f5f570d77a7e2012327827bcbb1
Aimazing <46224499+aimazingfeed@users.noreply.github.com>
2021-08-02 11:10:21 +0300

    type corrected

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -2,8 +2,8 @@ fun main() {
     val group_id = ""
     val access_token = ""
     while(true) {
-        var data = getLongPollServer(access_token, group_id)
-        var message = getMessage(data.server, data.key, data.ts, access_token)
+        val data = getLongPollServer(access_token, group_id)
+        val message = getMessage(data.server, data.key, data.ts, access_token)
     }
 }
 
